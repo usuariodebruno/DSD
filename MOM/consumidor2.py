4911306fcf5a2d031d76e669719a82134ed5ad80
Bruno Almeida <usuariodebruno@gmail.com>
2024-09-19 17:33:04 +0000

    add: implementa consumidores

--- MOM/consumidor2.py
+++ MOM/consumidor2.py
@@ -1,5 +1,7 @@
 import pika
 import json
+import random
+import time
 
 def consumir_pedido():
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
@@ -9,10 +11,11 @@ def consumir_pedido():
 
     def callback(channel, method, properties, body):
         pedido = json.loads(body)
-        print(f"[x] Processando pedido {pedido['pedido_id']}")
-        print(f"[x] Pedido {pedido['pedido_id']} do cliente {pedido['cliente']} no valor de {pedido['valor_total']} processado")
+        print(f"[x] Processando pedido {pedido['pedido_id']}")                 
+        time.sleep(random.randint(1, 5))
+        print(f"[x] Pedido {pedido['pedido_id']} do cliente {pedido['cliente']} no valor de {pedido['valor_total']} processado")  
 
-    # Consumidores competem para pegar mensagens da mesma fila
+    # Consumidores pegam mensagens da mesma fila 
     channel.basic_consume(queue='fila_pedidos', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Aguardando novos pedidos. Pressione CTRL+C para sair.')
